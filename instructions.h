5d02005ffa4c5daf7641d42b3d905124680f480f
Pucka41 <Simon.Dunze@bknrd.schule>
2022-11-08 11:29:33 +0100

    added memory allocation for the vitual ram and a little base for the cpu struct

---
+++ instructions.h
@@ -0,0 +1,12 @@
+//
+// Created by simon on 11/8/2022.
+//
+
+#ifndef GREYVM_C_INSTRUCTIONS_H
+#define GREYVM_C_INSTRUCTIONS_H
+
+#endif //GREYVM_C_INSTRUCTIONS_H
+#include "stdint.h"
+#include "stdlib.h"
+uint8_t* createMemory(int bytesize);
+void freeMemory(uint8_t* memory);
